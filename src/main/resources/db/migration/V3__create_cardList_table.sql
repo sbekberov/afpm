95ca571d909822b11fea310200a37ad077c8869d
sbekberov <s.bekberov@gmail.com>
2022-01-16 15:57:25 +0300

    Rename V20 migration name to right syntax

--- src/main/resources/db/migration/V3__create_cardList_table.sql
+++ src/main/resources/db/migration/V3__create_cardList_table.sql
@@ -4,8 +4,8 @@ CREATE TABLE card_list
     board_id     uuid REFERENCES board (id) ,
     updated_by   varchar(25)                NOT NULL,
     created_by   varchar(25)                NOT NULL,
-    created_date TIMESTAMP                  NOT NULL,
-    updated_date TIMESTAMP,
+    created_date DATE                 NOT NULL,
+    updated_date date,
     name         VARCHAR(200)               NOT NULL,
     archived     BOOLEAN                    NOT NULL DEFAULT FALSE
 );
