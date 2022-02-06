8155335c24d77814f7c694c2841ba00d1fc8220a
sbekberov <s.bekberov@gmail.com>
2022-02-06 14:19:40 +0200

    Replace extending Resource to Domain

--- src/main/java/spd/trello/controller/CommonController.java
+++ src/main/java/spd/trello/controller/CommonController.java
@@ -2,12 +2,12 @@ package spd.trello.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import spd.trello.domain.Resource;
+import spd.trello.domain.Domain;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface CommonController<E extends Resource> {
+public interface CommonController<E extends Domain> {
 
     ResponseEntity<E> create(E resource);
     ResponseEntity<E> update(UUID id, E resource);
