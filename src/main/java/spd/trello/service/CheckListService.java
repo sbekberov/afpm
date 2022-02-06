cfb52e3ed8caf8cf8dece4f42b2f717a94ab1cf7
sbekberov <s.bekberov@gmail.com>
2022-02-06 14:04:07 +0200

    Replace CheckListService

--- src/main/java/spd/trello/service/CheckListService.java
+++ src/main/java/spd/trello/service/CheckListService.java
@@ -1,13 +1,13 @@
 package spd.trello.service;
 
 import org.springframework.stereotype.Service;
-import spd.trello.domain.Checklist;
+import spd.trello.domain.CheckList;
 import spd.trello.repository.CRUDRepository;
 
 @Service
-public class CheckListService extends AbstractService<Checklist> {
+public class CheckListService extends AbstractService<CheckList> {
 
-    public CheckListService(CRUDRepository<Checklist> checklistRepository) {
+    public CheckListService(CRUDRepository<CheckList> checklistRepository) {
         super(checklistRepository);
     }
 
