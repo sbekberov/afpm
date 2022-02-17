71febfcab233abcd5d2550396060aad86a10c9d2
sbekberov <s.bekberov@gmail.com>
2022-02-17 19:30:46 +0200

    Added package "common"

--- src/main/java/spd/trello/domain/common/Resource.java
+++ src/main/java/spd/trello/domain/common/Resource.java
@@ -1,10 +1,12 @@
-package spd.trello.domain;
+package spd.trello.domain.common;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 import java.sql.Date;
 import java.time.LocalDate;
 
@@ -13,9 +15,14 @@ import java.time.LocalDate;
 @ToString(callSuper = true)
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
+@MappedSuperclass
 public abstract class Resource extends Domain  {
+    @Column(name = "created_by")
     String createdBy;
+    @Column(name = "updated_by")
     String updatedBy;
+    @Column(name = "created_date")
     Date createdDate=Date.valueOf(LocalDate.now());
+    @Column(name = "updated_date")
     Date updatedDate;
 }
