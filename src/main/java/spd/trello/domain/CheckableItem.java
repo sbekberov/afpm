d7bd048dca5d12af3c27e135167ce57cd35c1850
sbekberov <s.bekberov@gmail.com>
2022-02-18 17:01:40 +0200

    Delete extra import

--- src/main/java/spd/trello/domain/CheckableItem.java
+++ src/main/java/spd/trello/domain/CheckableItem.java
@@ -6,7 +6,7 @@ import spd.trello.domain.common.Domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import java.util.UUID;
+
 
 @Getter
 @Setter
