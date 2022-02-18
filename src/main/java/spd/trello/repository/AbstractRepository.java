d11811dc7f503b10dcc77d88fed05b4435fe88f5
sbekberov <s.bekberov@gmail.com>
2022-02-18 12:07:23 +0200

    Replace AbstractRepository

--- src/main/java/spd/trello/repository/AbstractRepository.java
+++ src/main/java/spd/trello/repository/AbstractRepository.java
@@ -4,10 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 import spd.trello.domain.common.Domain;
+import spd.trello.domain.common.Resource;
 
 import java.util.UUID;
 
 @NoRepositoryBean
-public interface AbstractRepository<E extends Domain> extends JpaRepository<E, UUID>,
+public interface AbstractRepository<E extends Resource> extends JpaRepository<E, UUID>,
         JpaSpecificationExecutor<E> {
 }
