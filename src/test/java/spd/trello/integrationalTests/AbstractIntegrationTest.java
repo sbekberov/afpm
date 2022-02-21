710c32ebb9c0a921b5ed44c08f9bbba0b4896034
sbekberov <s.bekberov@gmail.com>
2022-02-21 23:55:41 +0200

    Added integration tests

---
+++ src/test/java/spd/trello/integrationalTests/AbstractIntegrationTest.java
@@ -0,0 +1,67 @@
+package spd.trello.integrationalTests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import spd.trello.domain.common.Resource;
+
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+public abstract class AbstractIntegrationTest<E extends Resource> implements CommonIntegrationTest<E> {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper().findAndRegisterModules();
+
+    public Object getValue(MvcResult mvcResult, String jsonPath) throws UnsupportedEncodingException {
+        return JsonPath.read(mvcResult.getResponse().getContentAsString(), jsonPath);
+    }
+
+    @DisplayName("Get all")
+    public MvcResult getAll(String URL_TEMPLATE) throws Exception {
+        return mockMvc.perform(get(URL_TEMPLATE))
+                .andReturn();
+    }
+
+    @DisplayName("Save")
+    public MvcResult create(String URL_TEMPLATE, E entity) throws Exception {
+        return mockMvc.perform(MockMvcRequestBuilders.post(URL_TEMPLATE)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(entity)))
+                .andReturn();
+    }
+
+    @DisplayName("Delete")
+    public MvcResult delete(String URL_TEMPLATE, UUID id) throws Exception {
+        return mockMvc.perform(MockMvcRequestBuilders.delete(URL_TEMPLATE + "/{id}", id)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+    }
+
+    @DisplayName("Get by Id")
+    public MvcResult getById(String URL_TEMPLATE, UUID id) throws Exception {
+        return mockMvc.perform(MockMvcRequestBuilders.get(URL_TEMPLATE + "/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+    }
+
+    @DisplayName("Update")
+    public MvcResult update(String URL_TEMPLATE, UUID id, E entity) throws Exception {
+        return mockMvc.perform(MockMvcRequestBuilders.put(URL_TEMPLATE + "/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(entity)))
+                .andReturn();
+    }
+}
---
+++ src/test/java/spd/trello/integrationalTests/CommonIntegrationTest.java
@@ -0,0 +1,19 @@
+package spd.trello.integrationalTests;
+
+import org.springframework.test.web.servlet.MvcResult;
+import spd.trello.domain.common.Resource;
+
+import java.util.UUID;
+
+public interface CommonIntegrationTest<E extends Resource> {
+
+    MvcResult create(String urlTemplate, E entity) throws Exception;
+
+    MvcResult getAll(String urlTemplate) throws Exception;
+
+    MvcResult getById(String urlTemplate, UUID id) throws Exception;
+
+    MvcResult delete(String urlTemplate, UUID id) throws Exception;
+
+    MvcResult update(String urlTemplate, UUID id, E entity) throws Exception;
+}
