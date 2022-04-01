7b91594848d0d29cb4bbfcbe4fa042f36c69186f
s.bekberov <s.bekberov@gmail.com>
2022-04-01 18:46:28 +0300

    Added AttachmentDTO

---
+++ src/main/java/spd/trello/domain/dto/AttachmentDTO.java
@@ -0,0 +1,22 @@
+package spd.trello.domain.dto;
+
+import lombok.*;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class AttachmentDTO {
+    private UUID id;
+    private String createdBy;
+    private Date createdDate;
+    private String name;
+    private byte[] multiPartBytes;
+    private UUID cardId;
+    private boolean failed;
+}
