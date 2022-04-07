d2086f26d53bba2ef7bc314079cb0810585d50b9
s.bekberov <s.bekberov@gmail.com>
2022-04-07 17:13:15 +0300

    add path

--- src/main/java/spd/trello/service/AttachmentService.java
+++ src/main/java/spd/trello/service/AttachmentService.java
@@ -1,6 +1,7 @@
 package spd.trello.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import spd.trello.domain.Attachment;
