685cbd1f91c9600fc920104f8e5e1268824d17a0
s.bekberov <s.bekberov@gmail.com>
2022-04-30 11:10:28 +0300

    Added validation for Templates

--- src/main/java/spd/trello/domain/CardTemplate.java
+++ src/main/java/spd/trello/domain/CardTemplate.java
@@ -6,15 +6,17 @@ import spd.trello.domain.common.Resource;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 @Entity
 @Table
 public class CardTemplate extends Resource {
-    @Column(name = "name")
+    @Size(min = 2, max = 30, message = "The name field must be between 2 and 30 characters long.")
     private String name;
     @Column(name = "description")
+    @Size(min = 2, max = 255, message = "The description field must be between 2 and 255 characters long.")
     private String description;
 }
 
