7ba05c19fb407698ebbb42968865d68e0563dbfe
s.bekberov <s.bekberov@gmail.com>
2022-05-02 19:59:16 +0300

    Replacer HttpStatus

--- src/main/java/spd/trello/exception/FileCanNotBeUpload.java
+++ src/main/java/spd/trello/exception/FileCanNotBeUpload.java
@@ -3,7 +3,7 @@ package spd.trello.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.I_AM_A_TEAPOT, reason="File can`t be upload")
+@ResponseStatus(value= HttpStatus.BAD_REQUEST, reason="File can`t be upload")
 public class FileCanNotBeUpload extends RuntimeException {
 }
 
